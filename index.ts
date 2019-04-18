31886ce546daeb841205f12f7b5cda1e2b14fb65
Qchta <qchtapl@gmail.com>
2019-04-18 21:45:06 +0200

    2 files modified

--- index.ts
+++ index.ts
@@ -1,26 +1,33 @@
 import './style.css'
 import P5 from 'p5'
-
-
-import Qbee from './qbee'
+import { Qbee } from './qbee'
 
 
 const p5 = new P5(function (p5) {
 
-  
-  let b: Qbee = {p5:p5, x:0, y:0, z:0, size:50}
+  const dim = 3;
+  let cube: Qbee[][][];
 
   p5.setup = () => {
     p5.createCanvas(400, 400, p5.WEBGL);
     p5.frameRate(60);
 
+    cube = new Array(dim);
+    for(let i in [cube]) {
+      cube[i] = new Array(dim);
+      for(let j in cube[i]) {
+        cube[i][j] = new Array(dim);
+        for(let k in cube[j]) {
+          cube[i][j][k] = new Qbee(p5, Number(i), Number(j), Number(k), 50);
+        }
+      }
+    }
   }
 
   p5.draw = () => {
     p5.background(200);
     p5.rotateX(p5.frameCount * 0.01);
     p5.rotateY(p5.frameCount * 0.01);
-    b.draw();
   }
 })
 
--- qbee.ts
+++ qbee.ts
@@ -2,20 +2,13 @@ import P5 from 'p5'
 
 export class Qbee {
 
-  private p5: P5;
-
-  private x: number;
-  private y: number;
-  private z: number;
-  private size: number;
-
-  constructor(p5: P5, x: number, y: number, z: number, size: number) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
-    this.size = size;
-    this.p5 = p5;
-  }
+  constructor(
+    private p5: P5,
+    private x: number,
+    private y: number,
+    private z: number,
+    private size: number
+  ) {}
 
   public draw() {
     this.p5.box(50);
