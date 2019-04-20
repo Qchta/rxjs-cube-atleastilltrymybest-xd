97912ab00a2e58536c51f421418220baf6b321c4
Qchta <qchtapl@gmail.com>
2019-04-21 01:55:24 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import { Cubie, Layer } from './cubie'
 import './style.css'
 
 export enum Axis {
-  X,Y,Z
+  X, Y, Z
 }
 
 new P5(function (p5) {
@@ -14,10 +14,10 @@ new P5(function (p5) {
 
   let rotX = 0;
   let rotY = 0;
-  
+
   let rotXd = 0;
   let rotYd = 0;
-  
+
   let mouseX = 0;
   let mouseY = 0;
 
@@ -38,43 +38,73 @@ new P5(function (p5) {
   }
 
   p5.draw = () => {
+
+    if (p5.keyIsDown(p5.LEFT_ARROW)) {
+      rotY -= 5;
+    }
+
+    if (p5.keyIsDown(p5.RIGHT_ARROW)) {
+      rotY += 5;
+    }
+
+    if (p5.keyIsDown(p5.UP_ARROW)) {
+      rotX += 5;
+    }
+
+    if (p5.keyIsDown(p5.DOWN_ARROW)) {
+      rotX -= 5;
+    }
+
     p5.background(200);
     p5.scale(50);
     p5.rotateX(-p5.HALF_PI / 3);
     p5.rotateY(-p5.HALF_PI / 3);
     p5.push();
+
+    // let sinx = p5.sin((rotX + rotXd) * 0.01);
+    // let cosx = p5.cos((rotX + rotXd) * 0.01);
+    // let siny = p5.sin((rotY + rotYd) * 0.01);
+    // let cosy = p5.cos((rotY + rotYd) * 0.01);
+
+    // p5.applyMatrix(
+    //   cosy,       0,    -siny,      0,
+    //   -sinx*siny, cosx, -sinx*cosy, 0,
+    //   cosx*siny,  sinx, cosx*cosy,  0,
+    //   0,          0,    0,          1,
+    // )
+
     p5.rotateY((rotY + rotYd) * 0.01);
-    p5.rotateX((rotX + rotXd) * 0.01);
-    p5.push();
+    p5.rotateX(p5.cos(rotY + rotYd) * p5.cos(rotY + rotYd) * (rotX + rotXd) * 0.01);
+    p5.rotateZ(p5.sin(rotY + rotYd) * p5.sin(rotY + rotYd) * (rotX + rotXd) * 0.01);
+
     cube.forEach(qb => qb.draw());
-    p5.pop();
     p5.pop();
     drawHelpers();
   }
 
   p5.keyPressed = () => {
     switch (p5.key) {
-      case 'f': 
+      case 'f':
         rotate(Layer.FRONT, true); break;
       case 'F':
         rotate(Layer.FRONT, false); break;
-      case 'b': 
+      case 'b':
         rotate(Layer.BACK, false); break;
       case 'B':
         rotate(Layer.BACK, true); break;
-      case 'r': 
+      case 'r':
         rotate(Layer.RIGHT, true); break;
       case 'R':
         rotate(Layer.RIGHT, false); break;
-      case 'l': 
+      case 'l':
         rotate(Layer.LEFT, false); break;
       case 'L':
         rotate(Layer.LEFT, true); break;
-      case 'u': 
+      case 'u':
         rotate(Layer.UP, false); break;
       case 'U':
         rotate(Layer.UP, true); break;
-      case 'd': 
+      case 'd':
         rotate(Layer.DOWN, true); break;
       case 'D':
         rotate(Layer.DOWN, false); break;
@@ -101,12 +131,12 @@ new P5(function (p5) {
   function rotate(layer: Layer, clockwise: boolean) {
     from(cube).pipe(
       filter(qb => qb.currentState.layers.includes(layer)),
-      
-    ).forEach(qb => qb.rotate(layerToAxis(layer), clockwise)); 
+
+    ).forEach(qb => qb.rotate(layerToAxis(layer), clockwise));
   }
 
   function layerToAxis(layer: Layer): Axis {
-    switch(layer) {
+    switch (layer) {
       case Layer.FRONT:
       case Layer.BACK:
         return Axis.Z;
